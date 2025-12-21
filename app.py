0341ca6ee554a87f7a71c4626529bb92af293ca1
Isaiah <idjohnson2@miners.utep.edu>
2025-12-21 07:12:05 -0700

    Add basic Tkinter application window

---
+++ app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Book Annotator")
+    root.geometry("800x500")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
